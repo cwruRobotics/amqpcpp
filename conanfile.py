bd9b0834d36c3bdde807003611bd7c9b3afdeec4
Michael Schaffer <mls278@case.edu>
2016-11-21 15:53:17 -0500

    Add build and package methods

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,26 @@
-from conans import ConanFile
+from conans import ConanFile, CMake
 
 class GlenniferClientConan(ConanFile):
+   name = "amqp"
+   version = "1.0-snapshot"
+   url = "https://github.com/cwruRobotics/amqpcpp.git"
+   license = "MIT"
+   exports = "examples*", "include*", "src*", "CMakeLists.txt"
    settings = "os", "compiler", "build_type", "arch"
    requires = "librabbitmq/0.8.1@filonovpv/stable"
    generators = "cmake"
    build_policy = "missing"
    default_options = "librabbitmq:no_openssl=True"
+
+   def build(self):
+      cmake = CMake(self.settings)
+      self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
+      self.run("cmake --build . %s" % cmake.build_config)
+
+   def package(self):
+      self.copy("*.h", dst=".")
+      self.copy("*.lib", dst="lib", src="lib")
+      self.copy("*.a", dst="lib", src="lib")
+
+   def package_info(self):
+      self.cpp_info.libs = ["amqp"]
